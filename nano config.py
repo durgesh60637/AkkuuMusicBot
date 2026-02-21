2ac765fc33205e6d7ad898fd4db79de7f2448a10
durgesh60637 <rjddurgeshraj@gmail.com>
2026-02-21 08:18:57 +0530

    Create nano config.py

---
+++ nano config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL")
